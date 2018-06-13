e6de23301c04af3f6e14be65f3eb776a84de3ddb
amyyuqian <amyyuqian@gmail.com>
2018-06-13 11:45:52 -0400

    add update and delete user

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var session = require('express-session')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var userService = require('./services/user.service.server');
+
+mongoose.connect('mongodb://localhost/cs4550-node');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+    "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(session({
+ resave: false, 
+ saveUninitialized: true,
+ secret: 'shhh, dont tell anyone'
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+userService(app);
+
+app.listen(4000);
--- services/user.service.server.js
+++ services/user.service.server.js
@@ -5,17 +5,47 @@ export default function (app) {
   app.post('/api/register', register);
   app.post('/api/logout', logout);
   app.get('/api/profile', profile);
+  app.get('/api/user', findAllUsers);
+  app.get('/api/user/:userId', findUserById);
+  app.delete('/api/user/:userId', deleteUser)
+  app.put('/api/user/:userId', updateUser)
+}
+
+function findUserById(req, res) {
+  var id = req.params['userId'];
+  userModel.findUserById(id).then(function (user) {
+    res.json(user);
+  })
+}
+
+function deleteUser(req, res) {
+  var id = req.params['userId'];
+  userModel.deleteUser(id).then(function (user) {
+    res.json(user);
+  })
+}
+
+function updateUser(req, res) {
+  var id = req.params['userId'];
+  userMode.updateUser(id, req.body).then(function (user) {
+    res.json(user);
+  })
+}
+
+function profile(req, res) {
+  res.send(req.session['currentUser']);
+}
+
+function findAllUsers(req, res) {
+  userModel.findAllUsers().then(function (users) {
+    res.send(users);
+  })
 }
 
 function register(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-  var newUser = {
-    username: username,
-    password: password
-  };
+  var newUser = req.body;
   
-  userModel.findUserByUsername(username).then(function (user) {
+  userModel.findUserByUsername(newUser.username).then(function (user) {
     if(!user) {
       return userModel.createUser(newUser);
     }
