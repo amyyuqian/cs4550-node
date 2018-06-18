325fc5af98a05bb3ededc301a38efcc29d137727
amyyuqian <amyyuqian@gmail.com>
2018-06-18 09:11:17 -0400

    add isAdmin boolean to schema and add inactive after 30 min

--- models/user/user.schema.server.js
+++ models/user/user.schema.server.js
@@ -4,6 +4,7 @@ var userSchema = mongoose.Schema({
  password: String,
  firstName: String,
  lastName: String,
- email: String
+ email: String,
+ isAdmin: Boolean,
 }, {collection: 'user'});
 module.exports = userSchema;
--- server.js
+++ server.js
@@ -1,39 +1,50 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var session = require('express-session')
-var bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+var session = require("express-session");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
-var userService = require('./services/user.service.server');
+var userService = require("./services/user.service.server");
+var idleTimeoutSeconds = 1800;
 
-mongoose.connect('mongodb://heroku_sz86kh8d:3pl68eheog1ip31547bam8fa3q@ds259210.mlab.com:59210/heroku_sz86kh8d');
+mongoose.connect(
+  "mongodb://heroku_sz86kh8d:3pl68eheog1ip31547bam8fa3q@ds259210.mlab.com:59210/heroku_sz86kh8d"
+);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
-app.use(session({
- resave: false, 
- saveUninitialized: true,
- secret: 'shhh, dont tell anyone'
-}));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "shhh, dont tell anyone",
+    cookie: {
+      maxAge: idleTimeoutSeconds * 1000
+    },
+    rolling: true
+  })
+);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
 userService(app);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
